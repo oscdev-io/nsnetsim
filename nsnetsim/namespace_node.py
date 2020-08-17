7daf11e0dcc6c7324294cb531d5eef5df82e3385
Nigel Kukard <nkukard@lbsd.net>
2020-08-17 02:28:59 +0000

    Updated copyrights

--- nsnetsim/namespace_node.py
+++ nsnetsim/namespace_node.py
@@ -1,4 +1,7 @@
-# Copyright (C) 2019, AllWorldIT.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Copyright (C) 2019-2020, AllWorldIT.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
